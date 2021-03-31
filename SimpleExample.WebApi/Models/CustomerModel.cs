09a6c417918f41b5fd575c8254cd5fd4d898945f
andreisalvador <andreifs95@gmail.com>
2021-03-31 20:01:21 -0300

    Ajuste CustomerModel

--- SimpleExample.WebApi/Models/CustomerModel.cs
+++ SimpleExample.WebApi/Models/CustomerModel.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleExample.Domain;
+using System;
 
 namespace SimpleExample.WebApi.Models
 {
@@ -7,14 +8,16 @@ namespace SimpleExample.WebApi.Models
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public byte Age { get; private set; }
-        public CustomerModel(string firstName, string lastName, byte age)
+        public DateTime CreatedAt { get; private set; }
+        public CustomerModel(string firstName, string lastName, byte age, DateTime createdAt)
         {
             FirstName = firstName;
             LastName = lastName;
             Age = age;
+            CreatedAt = createdAt;
         }
 
         public static implicit operator CustomerModel(Customer customer)
-            => customer is null ? null : new CustomerModel(customer.FirstName, customer.LastName, customer.Age);
+            => customer is null ? null : new CustomerModel(customer.FirstName, customer.LastName, customer.Age, customer.CreatedAt);
     }
 }
